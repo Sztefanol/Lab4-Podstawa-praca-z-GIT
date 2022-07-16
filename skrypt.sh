45ab3dbfad76fb61414d2755b8727a24536e1e3b
Sztefanol <luk266@gmail.com>
2022-07-16 11:56:32 +0200

    implement --logs suporting an argument

--- skrypt.sh
+++ skrypt.sh
@@ -1,9 +1,21 @@
 case "$1" in
 	"--date")date;;
-	"--logs")for i in {1..100}
-			do
-				echo "log""$i"".txt" > log"$i".txt
-				echo "$0" >> log"$i".txt
-				date >> log"$i".txt
-			done;;
+	"--logs")i=1
+				if [ "$#" = "2" ]
+				then
+				while [ $i -le $2 ]
+				do
+					echo "log""$i"".txt" > log"$i".txt
+					echo "$0" >> log"$i".txt
+					date >> log"$i".txt
+					i=$[i + 1]
+				done
+				else
+				for i in {1..2}
+				do
+					echo "log""$i"".txt" > log"$i".txt
+					echo "$0" >> log"$i".txt
+					date >> log"$i".txt
+				done
+			 fi;;
 esac
